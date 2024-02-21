6874be7b0b55f6e63a9dc32306a93373bdc8d7e0
Cesar Briseño <142867500+CesarBr1397@users.noreply.github.com>
2024-02-21 14:40:59 -0600

    Add files via upload

---
+++ prueba_logica.java
@@ -0,0 +1,18 @@
+package logica;
+
+import igu.Pantalla;
+
+public class prueba_logica {
+
+    public static void main(String[] args) {
+        
+        Pantalla panta = new Pantalla ();           //Crear nueva pantalla (agregar "import igu.Pantalla")
+        panta.setVisible(true);                         //Mostrar pantalla
+        panta.setLocationRelativeTo(null);      //Se coloca null para que aparezca en el centro
+        
+        
+        
+        
+    }
+    
+}
